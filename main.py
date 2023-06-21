94b0f03020facd4bca8513ee2a3346b935c150ee
Rui Kurohara <cmt14044@ict.nitech.ac.jp>
2023-06-21 17:50:30 +0900

    change image

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from skimage.io import imread
 
 
 # download the image
-img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
+img_url = 'https://commons.wikimedia.org/wiki/File:Houousan_from_kaikomagatake_50_1996_9_15.jpg'
 
 im = imread(img_url)
 
